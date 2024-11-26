72d423b06037505396cf134465d0c3f14f728dc9
Aristia0 <maeline.duparc@gmail.com>
2024-11-26 11:51:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+k#include <stdio.h>
 #include <stdlib.h>
 
 enum piece { VIDE, PION, TOUR, CAVALIER, FOU, REINE, ROI };
@@ -26,4 +26,40 @@ struct partie{
     Couleur player;
 };
 typedef struct partie Partie;
+
+void affichage(Partie *partie)
+{
+    case_actuelle = partie->plate
+    if (partie->player == BLANC)
+    {
+        printf("C'est aux blancs de jouer.\n Veillez entrer un coup.\n");
+    }
+    else
+    {
+        printf("C'est aux noirs de jouer.\n Veillez entrer un coup.\n");
+    }
+    printf("+-+-+-+-+-+-+-+-+");
+    for (int i = 0, i < 8, i++)
+    {
+        for (int j = 0, j < 8, j++)
+        {
+            if (case_actuelle->c == BLANC)
+            {
+                int maj = 0;
+            }
+            else
+            {
+                int maj = 32;
+            }
+            printf("|");
+            switch (case_actuelle->p)
+            {
+                case 
+            }
+            //passage a la case suivante 
+        }
+        printf("|");
+    }
+}
+
 
