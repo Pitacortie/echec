1509ee0a4a121d4b34bae2a7f1016531fd04c2b4
Pitacortie <91468956+Pitacortie@users.noreply.github.com>
2024-12-18 22:31:10 +0100

    Update pve.c

--- pve.c
+++ pve.c
@@ -1,1 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
+struct possible{
+    int v;
+    int n;
+    Coup c;
+    struct possible *suivant;
+};
+typedef struct possible Possible;
+
+Possible *creer_possible(){
+    Possible *res = malloc(sizeof(Possible));
+    res->suivant = NULL;
+    return res;
+}
+
+Possible *max(Possible *p){
+    int max = p->v;
+    Possible *res = p;
+    while(p != NULL){
+        if(p->v > max){
+            max = p->v;
+            res = p;
+        }
+    p = p->suivant;
+    }
+    return res;
+}
+
+void end(Possible *p){
+    Possible *temp;
+    while(p != NULL){
+        temp = p->suivant;
+        free(p);
+        p = temp;
+    }
+}
+
+
+int a_jouer(Partie current, int n, int res, Possible l){
+    if(n ==3){
+        
+    }
+
+}
+
+int main(){
+    return 0;
+}
