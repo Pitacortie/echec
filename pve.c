7bd137a2dc352de3ac0d15532b51ee98a35f30ca
Pitacortie <91468956+Pitacortie@users.noreply.github.com>
2025-01-02 12:05:16 +0100

    Update pve.c

--- pve.c
+++ pve.c
@@ -6,7 +6,6 @@
 struct possible{
     int v;
     Coup c;
-    int *tab;
     struct possible *suivant;
 };
 typedef struct possible Possible;
@@ -61,12 +60,10 @@ int den_coup(Partie current, Possible *l){
                         temp.yFrom = y;
                         temp.xTo = x_bis;
                         temp.yTo = y_bis;
-                        int tab[200] = {0};
-                        if(verif_coup(&current, temp) != 0){
+                        if(verif_coup(&current, temp, 1) == 0 || verif_coup(&current, temp, 1) == 6){
                             suiv = creer_possible();
                             l->v = 0;
                             l->c = temp;
-                            l->tab = tab;
                             l->suivant = suiv;
                             l = l->suivant;
                             res++;
@@ -90,53 +87,74 @@ int max_tab(int *tab){
     return max;
 }
 
-int val_possible(Partie current, Coup c, int *tab, int n, int *k, Fiche *partie, int res, int i){
-    Coup *ech;
+int trad_verif_coup(Partie current, Coup c){
+    int verif = verif_coup(&current, c, 0);
+    if(verif == 0){
+        if(current.plateau[c.xTo][c.yTo].p != VIDE)
+            return 2;
+        else
+            return 1;
+    }
+    else if(verif == 6)
+        return 3;
+
+    return 0;
+    
+}
+
+int val_possible(Partie current, Coup c, int n, int res){
+    int tab[200] = {0};
+    int i = 0;
     Coup temp;
-    if(n >= 2){
-        return max_tab(tab);
+    if(n >= 5){
+        return res;
     }
     else{
-        jouer_coup(&current, c, k, ech, partie);
+        jouer_coup(&current, c);
         for(int x = 0; x < MAX_CASE; x++){
             for(int y = 0; y < MAX_CASE; y++){
                 if(current.plateau[x][y].p != VIDE && current.plateau[x][y].c == NOIR){
                     for(int x_bis = 0; x_bis < MAX_CASE; x_bis++){
                         for(int y_bis = 0; y_bis < MAX_CASE; y_bis++){
-                            printf("%d\n", i);
                             temp.xFrom = x;
                             temp.yFrom = y;
                             temp.xTo = x_bis;
                             temp.yTo = y_bis;
-                            int verif = verif_coup(&current, temp);
+                            int verif = trad_verif_coup(current, temp);
                             switch(verif){
-                                case 0:
-                                    tab[i] = 0;
+                                case 1:
+                                    tab[i] = res + 1;
+                                    i++;
+                                    res = val_possible(current, temp, n+1, res);
                                 break;
-                                default:
-                                    if(verif == 1)
-                                        tab[i] = res + 1;
-                                    if(verif == 2)
+                                case 2:
+                                    if(current.plateau[c.xFrom][c.yFrom].c == NOIR)
                                         tab[i] = res + 2;
-                                    if(est_echec(&current, k[0], k[1], NOIR, ech))
-                                        tab[i] = res + 10;
-                                    if(est_echec(&current, k[2], k[3], BLANC, ech))
-                                        tab[i] = res - 10;
-                                    printf("ok\n");
-                                    if(est_echec(&current, k[2], k[3], BLANC, ech) && est_mat(&current, k, ech) == 1){
-                                        printf("ok2\n");
-                                        if(current.plateau[ech->xFrom][ech->yFrom].c == BLANC){
-                                            tab[i] = res - 100;
-                                        }                              
-                                        else{
-                                            tab[i] = res + 100;
-                                        }
-                                    }
+                                    else
+                                        tab[i] = res - 2;
+                                    i++;
+                                    res = val_possible(current, temp, n+1, res);
                                 break;
-                            }
-                            i++;
+                                case 3:
+                                    if(current.Noir.echec == 1){
+                                        if(current.Noir.mat == 1)
+                                            tab[i] = res -100;
+                                        else
+                                            tab[i] = res - 10;
+                                    }
+                                    else{
+                                        if(current.Blanc.mat == 1)
+                                            tab[i] = res + 100;
+                                        else    
+                                            tab[i] = res + 10;
+                                    }
+                                    i++;
+                                    res = val_possible(current, temp, n+1, res);
+                                break;
 
-                            res = val_possible(current,temp, tab, n+1, k, partie, tab[i], i);
+                            }
+
+                            
                         }
                     }
                 }
@@ -148,18 +166,19 @@ int val_possible(Partie current, Coup c, int *tab, int n, int *k, Fiche *partie,
 
 
 int main(){
-    Partie test;
-    int *k = king();
-    Fiche *p = creer_maillon();
-    test.plateau = creer_plateau();
+    Partie test =creer_partie();
     Possible *list = creer_possible();
     Possible *begin = list;
     int den = den_coup(test, list);
-    int tab[20000] = {0};
     for(int i = 0; i < den - 1; i++){
-        list->v = val_possible(test, list->c, tab, 0, k, p, 0, 0);
+        list->v = val_possible(test, list->c, 0, 0);
         list = list->suivant;
+
     }
     printf("%d\n", max_chain(begin)->v);
+    printf("%d\n", max_chain(begin)->c.xFrom);
+    printf("%d\n", max_chain(begin)->c.yFrom);
+    printf("%d\n", max_chain(begin)->c.xTo);
+    printf("%d\n", max_chain(begin)->c.yTo);
     return 0;
 }
